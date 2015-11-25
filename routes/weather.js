b5b4ab311595ba6ce671d890951737836a74dfce
Samer Albahra <salbahra@gmail.com>
2015-11-25 03:22:26 -0600

    Fix default value for users with Auto Rain Delay method

--- routes/weather.js
+++ routes/weather.js
@@ -415,7 +415,7 @@ exports.getWeather = function( req, res ) {
 				// If it is raining and the user has weather-based rain delay as the adjustment method then apply the specified delay
 				if ( adjustmentMethod === 2 ) {
 
-					rainDelay = ( adjustmentOptions && adjustmentOptions.hasOwnProperty( "d" ) ) ? adjustmentOptions.d : 1440;
+					rainDelay = ( adjustmentOptions && adjustmentOptions.hasOwnProperty( "d" ) ) ? adjustmentOptions.d : 24;
 				} else {
 
 					// For any other adjustment method, apply a scale of 0 (as the scale will revert when the rain stops)
