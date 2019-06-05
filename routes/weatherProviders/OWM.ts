dcc6413248e9ba00bc712b1c2168b38caffc1b5b
Matthew Oslan <derpthemeus@gmail.com>
2019-06-05 12:56:27 -0400

    Add ETo support to OWM WeatherProvider

--- routes/weatherProviders/OWM.ts
+++ routes/weatherProviders/OWM.ts
@@ -1,5 +1,7 @@
-import { GeoCoordinates, WateringData, WeatherData, WeatherProvider } from "../../types";
+import { EToData, GeoCoordinates, WateringData, WeatherData, WeatherProvider } from "../../types";
 import { httpJSONRequest } from "../weather";
+import * as moment from "moment-timezone";
+import { approximateSolarRadiation, CloudCoverInfo } from "../adjustmentMethods/EToAdjustmentMethod";
 
 async function getOWMWateringData( coordinates: GeoCoordinates ): Promise< WateringData > {
 	const OWM_API_KEY = process.env.OWM_API_KEY,
@@ -87,8 +89,53 @@ async function getOWMWeatherData( coordinates: GeoCoordinates ): Promise< Weathe
 	return weather;
 }
 
+async function getOWMEToData( coordinates: GeoCoordinates ): Promise< EToData > {
+	const OWM_API_KEY = process.env.OWM_API_KEY,
+		forecastUrl = "http://api.openweathermap.org/data/2.5/forecast?appid=" + OWM_API_KEY + "&units=imperial&lat=" + coordinates[ 0 ] + "&lon=" + coordinates[ 1 ];
+
+	// Perform the HTTP request to retrieve the weather data
+	let forecast;
+	try {
+		forecast = await httpJSONRequest( forecastUrl );
+	} catch (err) {
+		// Indicate ETo data could not be retrieved if an API error occurs.
+		return undefined;
+	}
+
+	// Indicate ETo data could not be retrieved if the forecast data is incomplete.
+	if ( !forecast || !forecast.list || forecast.list.length < 8 ) {
+		return undefined;
+	}
+
+	// Take a sample over 24 hours.
+	const samples = forecast.list.slice( 0, 8 );
+
+	const cloudCoverInfo: CloudCoverInfo[] = samples.map( ( window ): CloudCoverInfo => {
+		return {
+			startTime: moment.unix( window.dt ),
+			endTime: moment.unix( window.dt ).add( 3, "hours" ),
+			cloudCover: window.clouds.all / 100
+		};
+	} );
+
+	return {
+		weatherProvider: "OWM",
+		minTemp: samples.reduce( ( min, window ) => Math.min( min, window.main.temp ), 1000),
+		maxTemp: samples.reduce( ( max, window ) => Math.max( max, window.main.temp ), -1000),
+		minHumidity: samples.reduce( ( min, window ) => Math.min( min, window.main.humidity ), 100),
+		maxHumidity: samples.reduce( ( max, window ) => Math.max( max, window.main.humidity ), 0),
+		solarRadiation: approximateSolarRadiation( cloudCoverInfo, coordinates ),
+		// Assume wind speed measurements are taken at 2 meters.
+		windSpeed: samples.reduce( ( sum, window ) => sum + window.wind.speed, 0) / samples.length,
+		dayOfYear: moment().dayOfYear(),
+		// OWM always returns precip in mm, so it must be converted.
+		precip: samples.reduce( ( sum, window ) => sum + ( window.rain ? window.rain["3h"] || 0 : 0 ), 0) / 25.4
+	};
+}
+
 const OWMWeatherProvider: WeatherProvider = {
 	getWateringData: getOWMWateringData,
-	getWeatherData: getOWMWeatherData
+	getWeatherData: getOWMWeatherData,
+	getEToData: getOWMEToData
 };
 export default OWMWeatherProvider;
