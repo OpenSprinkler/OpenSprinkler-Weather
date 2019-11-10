0fae1002b5bb78fbb18ab5799cd861d9f170fe94
Pete ba <pete_ba@yahoo.com>
2019-11-10 22:36:19 +0000

    Initial commit

--- routes/adjustmentMethods/RainDelayAdjustmentMethod.ts
+++ routes/adjustmentMethods/RainDelayAdjustmentMethod.ts
@@ -1,13 +1,18 @@
 import { AdjustmentMethod, AdjustmentMethodResponse, AdjustmentOptions } from "./AdjustmentMethod";
-import { GeoCoordinates, ZimmermanWateringData } from "../../types";
+import { GeoCoordinates, PWS, ZimmermanWateringData } from "../../types";
 import { WeatherProvider } from "../weatherProviders/WeatherProvider";
 
 
 /**
  * Only delays watering if it is currently raining and does not adjust the watering scale.
  */
-async function calculateRainDelayWateringScale( adjustmentOptions: RainDelayAdjustmentOptions, coordinates: GeoCoordinates, weatherProvider: WeatherProvider ): Promise< AdjustmentMethodResponse > {
-	const wateringData: ZimmermanWateringData = await weatherProvider.getWateringData( coordinates );
+async function calculateRainDelayWateringScale(
+	adjustmentOptions: RainDelayAdjustmentOptions,
+	coordinates: GeoCoordinates,
+	weatherProvider: WeatherProvider,
+	pws?: PWS
+): Promise< AdjustmentMethodResponse > {
+	const wateringData: ZimmermanWateringData = await weatherProvider.getWateringData( coordinates, pws );
 	const raining = wateringData && wateringData.raining;
 	const d = adjustmentOptions.hasOwnProperty( "d" ) ? adjustmentOptions.d : 24;
 	return {
