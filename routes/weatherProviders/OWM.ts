16533e455b1b7902bed6b95687269810c5087c8f
Nathan P. <ncp294@gmail.com>
2025-07-02 16:33:56 -0400

    Reduction of Zimmerman and ETo data into one function for OWM.

--- routes/weatherProviders/OWM.ts
+++ routes/weatherProviders/OWM.ts
@@ -1,9 +1,8 @@
-import { GeoCoordinates, PWS, WeatherData, ZimmermanWateringData } from "../../types";
+import { GeoCoordinates, PWS, WeatherData, WateringData } from "../../types";
 import { httpJSONRequest, keyToUse } from "../weather";
 import { WeatherProvider } from "./WeatherProvider";
-import { approximateSolarRadiation, CloudCoverInfo, EToData } from "../adjustmentMethods/EToAdjustmentMethod";
+import { approximateSolarRadiation, CloudCoverInfo } from "../adjustmentMethods/EToAdjustmentMethod";
 import * as moment from "moment";
-import * as geoTZ from "geo-tz";
 import { CodedError, ErrorCode } from "../../errors";
 
 export default class OWMWeatherProvider extends WeatherProvider {
@@ -15,49 +14,71 @@ export default class OWMWeatherProvider extends WeatherProvider {
 		this.API_KEY = process.env.OWM_API_KEY;
 	}
 
-	// public async getWateringData(coordinates: GeoCoordinates, pws?: PWS): Promise<ZimmermanWateringData[]> {
+	public async getWateringData(coordinates: GeoCoordinates, pws?: PWS): Promise<WateringData[]> {
 
-	// 	const localKey = keyToUse(this.API_KEY, pws);
+		const localKey = keyToUse(this.API_KEY, pws);
 
-	// 	// The OWM free API options changed so need to use the new API method
-	// 	//Get previous date by using UTC
-	// 	const timezone = moment().tz( geoTZ( coordinates[ 0 ], coordinates[ 1 ] )[ 0 ] ).utcOffset();
-	// 	let time = Date.now();
-	// 	time -= (86400000 + timezone * 3600);
-	// 	const date = new Date(time);
-	// 	let day = this.pad(date.getUTCDate());
-	// 	let month = this.pad(date.getUTCMonth() + 1);
-	// 	const yesterdayUrl = `https://api.openweathermap.org/data/3.0/onecall/day_summary?units=imperial&appid=${ localKey }&lat=${ coordinates[ 0 ] }&lon=${ coordinates[ 1 ] }&date=${date.getUTCFullYear()}-${month}-${day}`;
-	// 	const todayUrl = `https://api.openweathermap.org/data/3.0/onecall?units=imperial&lat=${ coordinates[ 0 ] }&lon=${ coordinates[ 1 ] }&exclude=minutely,hourly,daily,alerts&appid=${ localKey }`;
+		//Get previous date by using UTC
+		const yesterdayTimestamp: string = moment().subtract( 1, "day" ).utc().format("YYYY-MM-DD");
 
-	// 	// Perform the HTTP request to retrieve the weather data
-	// 	let yesterdayData, todayData;
-	// 	try {
-	// 		yesterdayData = await httpJSONRequest(yesterdayUrl);
-	// 		todayData = await httpJSONRequest(todayUrl);
+		const yesterdayUrl = `https://api.openweathermap.org/data/3.0/onecall/day_summary?units=imperial&appid=${ localKey }&lat=${ coordinates[ 0 ] }&lon=${ coordinates[ 1 ] }&date=${yesterdayTimestamp}`;
+		const todayUrl = `https://api.openweathermap.org/data/3.0/onecall?units=imperial&lat=${ coordinates[ 0 ] }&lon=${ coordinates[ 1 ] }&exclude=minutely,hourly,daily,alerts&appid=${ localKey }`;
 
-	// 	} catch ( err ) {
-	// 		console.error( "Error retrieving weather information from OWM:", err );
-	// 		throw new CodedError( ErrorCode.WeatherApiError );
-	// 	}
+		// Perform the HTTP request to retrieve the weather data
+		let historicData, todayData;
+		try {
+			historicData = await httpJSONRequest(yesterdayUrl);
+			todayData = await httpJSONRequest(todayUrl);
 
-	// 	// Indicate watering data could not be retrieved if the forecast data is incomplete.
-	// 	if ( !yesterdayData || !todayData ) {
-	// 		throw new CodedError( ErrorCode.MissingWeatherField );
-	// 	}
+		} catch ( err ) {
+			console.error( "Error retrieving weather information from OWM:", err );
+			throw new CodedError( ErrorCode.WeatherApiError );
+		}
 
-	// 	let temp = yesterdayData.temperature;
+		// Indicate watering data could not be retrieved if the forecast data is incomplete.
+		if ( !historicData || !todayData ) {
+			throw new CodedError( ErrorCode.MissingWeatherField );
+		}
 
-	// 	let totalTemp = temp.min + temp.max + temp.afternoon + temp.night + temp.evening + temp.morning;
+		let clouds = [historicData.cloud_cover.afternoon, todayData.current.clouds];
 
-	// 	return [{
-	// 		weatherProvider: "OWM",
-	// 		temp: totalTemp / 6,
-	// 		humidity: (yesterdayData.humidity.afternoon + todayData.current.humidity) / 2,
-	// 		precip: yesterdayData.precipitation.total / 25.4,
-	// 		raining: (todayData.current.weather.main === "Rain")
-	// 	}];
-	// }
+		const cloudCoverInfo: CloudCoverInfo[] = clouds.map( ( sample ): CloudCoverInfo => {
+			if( sample === undefined ) {
+				return {
+					startTime: moment(),
+					endTime: moment(),
+					cloudCover: 0
+				}
+			}
+			return {
+				startTime: moment(),
+				endTime: moment().add( 1, "hours" ),
+				cloudCover: sample / 100
+			}
+		});
+
+		let temp = historicData.temperature;
+
+		let totalTemp = temp.min + temp.max + temp.afternoon + temp.night + temp.evening + temp.morning;
+
+		return [{
+			weatherProvider: "OWM",
+			temp: totalTemp / 6,
+			humidity: (historicData.humidity.afternoon + todayData.current.humidity) / 2,
+			// OWM always returns precip in mm, so it must be converted.
+			precip: historicData.precipitation.total / 25.4,
+			raining: (todayData.current.weather.main === "Rain"),
+			periodStartTime: Number(moment().subtract(1, "day").format("X")),
+			minTemp: (historicData.temperature.min < todayData.current.temp ? historicData.temperature.min : todayData.current.temp),
+			maxTemp: (historicData.temperature.max > todayData.current.temp ? historicData.temperature.max : todayData.current.tmep),
+			minHumidity: (historicData.humidity.afternoon < todayData.current.humidity ? historicData.humidity.afternoon : todayData.current.humidity),
+			maxHumidity: (historicData.humidity.afternoon > todayData.current.humidity ? historicData.humidity.afternoon : todayData.current.humidity),
+			solarRadiation: approximateSolarRadiation( cloudCoverInfo, coordinates ),
+			// Assume wind speed measurements are taken at 2 meters.
+			// Use current wind speed as previous day only returns max for the day
+			windSpeed: todayData.current.wind_speed,
+		}];
+	}
 
 	public async getWeatherData(coordinates: GeoCoordinates, pws?: PWS): Promise<WeatherData> {
 
@@ -108,65 +129,6 @@ export default class OWMWeatherProvider extends WeatherProvider {
 		}
 
 		return weather;
-	}
-
-	async getEToData(coordinates: GeoCoordinates, pws?: PWS): Promise<EToData[]> {
-
-		const localKey = keyToUse(this.API_KEY, pws);
-
-		//Get previous date by using UTC
-		const yesterdayTimestamp: string = moment().subtract( 1, "day" ).utc().format("YYYY-MM-DD");
-
-		const historicUrl = `https://api.openweathermap.org/data/3.0/onecall/day_summary?units=imperial&appid=${ localKey }&lat=${ coordinates[ 0 ] }&lon=${ coordinates[ 1 ] }&date=${yesterdayTimestamp}`;
-		const todayUrl = `https://api.openweathermap.org/data/3.0/onecall?units=imperial&lat=${ coordinates[ 0 ] }&lon=${ coordinates[ 1 ] }&exclude=minutely,hourly,daily,alerts&appid=${ localKey }`;
-
-
-		// Perform the HTTP request to retrieve the weather data
-		let historicData, todayData;
-		try {
-			historicData = await httpJSONRequest(historicUrl);
-			todayData = await httpJSONRequest(todayUrl);
-		} catch (err) {
-			console.error( "Error retrieving ETo information from OWM:", err );
-			throw new CodedError( ErrorCode.WeatherApiError );
-		}
-
-		// Indicate ETo data could not be retrieved if the forecast data is incomplete.
-		if ( !historicData || !todayData ) {
-			throw new CodedError( ErrorCode.InsufficientWeatherData );
-		}
-
-		let clouds = [historicData.cloud_cover.afternoon, todayData.current.clouds];
-
-		const cloudCoverInfo: CloudCoverInfo[] = clouds.map( ( sample ): CloudCoverInfo => {
-			if( sample === undefined ) {
-				return {
-					startTime: moment(),
-					endTime: moment(),
-					cloudCover: 0
-				}
-			}
-			return {
-				startTime: moment(),
-				endTime: moment().add( 1, "hours" ),
-				cloudCover: sample / 100
-			}
-		})
-
-		return [{
-			weatherProvider: "OWM",
-			periodStartTime: Number(moment().subtract(1, "day").format("X")),
-			minTemp: (historicData.temperature.min < todayData.current.temp ? historicData.temperature.min : todayData.current.temp),
-			maxTemp: (historicData.temperature.max > todayData.current.temp ? historicData.temperature.max : todayData.current.tmep),
-			minHumidity: (historicData.humidity.afternoon < todayData.current.humidity ? historicData.humidity.afternoon : todayData.current.humidity),
-			maxHumidity: (historicData.humidity.afternoon > todayData.current.humidity ? historicData.humidity.afternoon : todayData.current.humidity),
-			solarRadiation: approximateSolarRadiation( cloudCoverInfo, coordinates ),
-			// Assume wind speed measurements are taken at 2 meters.
-			// Use current wind speed as previous day only returns max for the day
-			windSpeed: todayData.current.wind_speed,
-			// OWM always returns precip in mm, so it must be converted.
-			precip: historicData.precipitation.total / 25.4
-		}];
 	}
 
 	pad(number: number){
