a911c87b8b2324d00debd1cbc604ba501065c95b
arfrie22 <43021241+arfrie22@users.noreply.github.com>
2025-08-14 11:50:47 -0400

    clean up server.ts

--- server.ts
+++ server.ts
@@ -3,31 +3,12 @@ import 'dotenv/config'
 import express from "express";
 import cors from "cors";
 
-import { getTZ, getWateringData, getWeatherData, localTime, resolveCoordinates } from "./routes/weather";
+import { getWateringData, getWeatherData } from "./routes/weather";
 import { captureWUStream } from "./routes/weatherProviders/local";
 import { getBaselineETo } from "./routes/baselineETo";
 import {default as packageJson} from "./package.json";
 import { pinoHttp } from "pino-http";
 import { pino, LevelWithSilent } from "pino";
-
-import SunCalc from "suncalc";
-
-
-async function test() {
-    let coords = await resolveCoordinates('01002');
-
-
-let a = SunCalc.getTimes(
-        new Date(),
-        coords[0],
-        coords[1]
-    );
-
-    console.log(((a.dusk.getTime()) - (a.dawn.getTime())) / 1000);
-}
-
-test();
-
 
 function getLogLevel(): LevelWithSilent {
     switch (process.env.LOG_LEVEL) {
